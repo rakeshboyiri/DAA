e36a7a35450892af6b22d51026617cfb01e68b86
rakeshboyiri <boyirirakesh@gmail.com>
2024-07-15 00:53:56 +0530

    files-added

---
+++ cpp/test.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+int n ;
+using namespace std;
+int val[100]={1,2,3,4,5,6};;
+int wt [100] = {1,2,3,4,5,6};;
+int main(){
+    n = 6;
+    
+    a[n];
+    ks(n,0,)
+
+    return 0;
+}
